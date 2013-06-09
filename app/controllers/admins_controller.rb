097c788c2173c631cc5e71ae055811278a96aac2
Madhawa Chandrasena <dilmadhawa@gmail.com>
2013-06-09 12:50:04 +0530

    Comment added app/controllers/admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,7 @@
 class AdminsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :redirect_unless_admin
-
+#test comment
   def user_search
     params[:user] ||= {}
     params[:user].delete_if {|key, value| value.blank? }
