a11a37fcdeb64f35592664c4c25b5b3d42e9558a
sandaruwan <sandaruwan.sp@gmail.com>
2013-11-18 16:54:09 +0530

    Added test methods for api_controller.rb

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -8,17 +8,48 @@ describe Api::UsersController do
     @controller.stub(:current_user).and_return(@user)
   end
 
- 
-  describe "routing to profiles" do
-    it "routes /profile/:username to profile#show for username" do
-        get :getPodPersonList
-         
-        #:controller => "Users",
-        #:action => "show",
-        #:id => "1",
-        #:access_token => "919a8164107702dca99bfc9db495ffab"
+  describe "Accessing api methods" do
+    it "without an access token'" do
+        @expected = {:error => "300"}.to_json
+        get 'getUserpersonList'
+        response.body.should == @expected
+    end
 
+    it "with an invalid access token" do
+        @expected = {:error => "300"}.to_json
+        get 'getUserpersonList' ,{'access_token'=> "100"}
+        response.body.should == @expected
+    end
+
+    it "without profile read permission" do
+        @expected = {:error => "310"}.to_json
+        @rt = FactoryGirl.create(:refresh_token)
+        @at = FactoryGirl.create(:access_token, :refresh_token => @rt.token)
+        get 'getUserpersonList' ,{ 'access_token' => @at.token }
+        response.body.should == @expected
     end
   end
+ 
+#  describe "Call for api methods without access token" do
+#    it "routes 'getUserpersonList/:diaspora_handle/:access_token'" do
+#        @expected = { 
+#        	:flashcard  => @flashcard,
+#        	:lesson     => @lesson,
+#        	:success    => true
+#        }.to_json
+#
+#        rt = FactoryGirl.create(:refresh_token)
+#	at = FactoryGirl.create(:access_token, :refresh_token => rt.token)
+#        
+#        #//@refresh_tuple=FactoryGirl.create(:refresh_token)
+#       # @access_tuple=FactoryGirl.create(:access_token)
+# 	
+#        get 'getUserpersonList' ,{'access_token'=>at.token}
+#        response.body.should == @expected
+#    end
+#    it "routes 'getUserpersonList/:diaspora_handle/:access_token'" do
+#        get :getUserpersonList      
+#    end
+#  end
 
 end
