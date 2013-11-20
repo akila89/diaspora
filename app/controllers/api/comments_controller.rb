df6064dfc6ea2d363d419abface72550fa534361
sandaruwan <sandaruwan.sp@gmail.com>
2013-11-20 17:21:49 +0530

    Modified comments_controller api class

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,35 +1,13 @@
 class Api::CommentsController < Api::ApiController
 
   before_filter :require_comment_read_permision, :only => [:get_given_user_comment_list,
-                                                           :get_given_user_comment_list_by_handle,
                                                            :get_likes_count,
-							   :index,
-							   :show,
-							   :create_comment]
-  #before_filter :require_comment_delete_permision, :only => [:deleteComment]
+							   ]
 
-  before_filter :fetch_user, :except => [:index, :create]
+  before_filter :require_comment_write_permision, :only => [:create_comment]
 
-  def fetch_user
-    @comment = Comment.find_by_id(params[:id])
-  end
+  before_filter :require_comment_delete_permision, :only => [:delete_Comment]
 
-# Can retrieve all the comments in the current pod
-  def index
-    @comments = Comment.all
-    respond_to do |format|
-      format.json { render json: @comments }
-      format.xml { render xml: @comments }
-    end
-  end
-
-# Can retrieve comment specified by the id
-  def show
-    respond_to do |format|
-      format.json { render json: @comment }
-      format.xml { render xml: @comment }
-    end
-  end
 
 # Can retrieve all comments posted by given user
 def get_given_user_comment_list
@@ -43,14 +21,9 @@ def get_given_user_comment_list
         	@comments_array.push @comment	
 	 end  
        end
-    respond_to do |format|
-      format.json { render json: @comments_array }
-      format.xml { render xml: @comments_array }
-    end
+	render :status => :response, :json => {:user_comment_list => @comments_array}
     else
-    respond_to do |format|
-      format.json { render :status => :bad_request, :json => {:error => 500}}
-    end
+	render :status => :bad_request, :json => {:error => "400"}
     end
   end
 
@@ -61,19 +34,12 @@ def get_given_user_comment_list
     if @person && @comment
       if @person.diaspora_handle==@comment.diaspora_handle
 	        @likes_count = {likes_count: @comment.likes_count.nil? ? "": @comment.likes_count}.to_json	
-      respond_to do |format|
-        format.json { render json: @likes_count }
-        format.xml { render xml: @likes_count }
-      end
+	render :status => :response, :json => {:likes_count => @likes_count}
       else
-          respond_to do |format|
-            format.json { render :status => :bad_request, :json => {:error => 501}}  # incompatible data
-          end
+	render :status => :bad_request, :json => {:error => "403"}
       end
     else
-    respond_to do |format|
-      format.json { render :status => :bad_request, :json => {:error => 500}}
-    end
+	render :status => :bad_request, :json => {:error => "400"}
     end
   end
 
@@ -92,9 +58,7 @@ def get_given_user_comment_list
         render :nothing => true
       end
     else
-    respond_to do |format|
-      format.json { render :status => :bad_request, :json => {:error => 500}}
-    end
+	render :status => :bad_request, :json => {:error => "400"}
     end
   end
 
@@ -107,34 +71,15 @@ def get_given_user_comment_list
     @comment = Comment.find(params[:id])
     if @user.owns?(@comment) || @user.owns?(@comment.parent)
       @user.retract(@comment)
-      respond_to do |format|
-        format.json { render :nothing => true }
-      end
+	render :nothing => true
     else
-          respond_to do |format|
-            format.json { render :status => :bad_request, :json => {:error => 501}}  # incompatible data
-          end
+	render :status => :bad_request, :json => {:error => "403"}
     end
     else
-    respond_to do |format|
-      format.json { render :status => :bad_request, :json => {:error => 500}}
-    end
+	render :status => :bad_request, :json => {:error => "400"}
     end
   end
 
 end
-
-
-#API Requests:
-
-#=> listing comments
-#   url: http://localhost:3000/api/comments
-#   method: GET
-
-
-#=> Retrieving comment detail
-#  url: http://localhost:3000/api/comments/:id 
-#  method: GET
-
 
 
