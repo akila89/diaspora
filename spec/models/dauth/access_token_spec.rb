41e07c0b8ce5182e4bc565e9ce80cc8711e9beda
akilai <iroshan.akila@gmail.com>
2013-12-06 21:56:56 +0530

    Add tests for access token model using factory.

--- spec/models/dauth/access_token_spec.rb
+++ spec/models/dauth/access_token_spec.rb
@@ -2,33 +2,36 @@ require 'spec_helper'
 
 describe Dauth::AccessToken do
   before { 
-  @accesstoken = Dauth::AccessToken.new(
-      refresh_token: "refresh_token",
-      secret: "access_secret",
-      token: "access_token"
-      )}
-      
-  subject { @accesstoken }
+    @access_token = FactoryGirl.create(:access_token)
+  }
   
-  it { should respond_to(:refresh_token) }
-  it { should respond_to(:secret) }
-  it { should respond_to(:token) }
-  it { should be_valid }
-  
-  describe "when refresh token is not present" do
-    before{ @accesstoken.refresh_token ="" }
-    it {should_not be_valid }
-  end
-  
-  #TODO refresh_token uniqueness validate
-  
-  describe "when secret is not present" do
-    before{ @accesstoken.secret ="" }
-    it {should_not be_valid }
-  end
-  
-  describe "when access token is not present" do
-    before{ @accesstoken.token ="" }
-    it {should_not be_valid }
+  describe "validation" do
+    
+    describe "of object from fractory" do
+      it "must be ok" do
+        @access_token.should be_valid
+      end
+    end
+    
+    describe "of token" do
+      describe "requires presence" do
+        before{ @access_token.token ="" }
+        it {@access_token.should_not be_valid}
+      end   
+    end
+    
+    describe "of refresh_token" do
+      describe "requires presence" do
+        before{ @access_token.refresh_token = nil}
+        it {@access_token.should_not be_valid}
+      end   
+    end
+    
+    describe "of secret" do
+      describe "requires presence" do
+        before{ @access_token.secret ="" }
+        it {@access_token.should_not be_valid}
+      end   
+    end
   end
 end
